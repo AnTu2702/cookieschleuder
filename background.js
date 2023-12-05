586a7523d35418a6910e8bbb063838c0f53a9407
Andreas Tully <andreas.tully@deutschebahn.com>
2023-12-05 14:30:56 +0100

    Update Manifest to V3

--- background.js
+++ background.js
@@ -3,22 +3,27 @@
 var cookie = 'cookied_8080';
 var port = '8080';
 
-chrome.storage.sync.get({
-    cookie: cookie,
-    port: port
-  }, function(items) {
-    cookie = items.cookie;
-    port = items.port;
-});
+chrome.cookies.onChanged.addListener(async (changeInfo) => {
 
-chrome.cookies.onChanged.addListener(function(changeInfo) {
+    chrome.storage.sync.get({
+      cookie: cookie,
+      port: port
+    }, function(items) {
+      cookie = items.cookie;
+      port = items.port;
+    });
+    
+    if ((changeInfo.cookie.name.localeCompare(cookie) == 0) & ((changeInfo.cause == "expired_overwrite") | (changeInfo.cause == "explicit"))) {
 
-    if ((changeInfo.cookie.name == cookie) & ((changeInfo.cause == "expired_overwrite") | (changeInfo.cause == "explicit"))) {
-      var xhr = new XMLHttpRequest();
-      xhr.open("PUT", "http://127.0.0.1:"+port+"/"+cookie, true);
-      xhr.send(changeInfo.cookie.value);
+      await fetch("http://127.0.0.1:"+port+"/"+cookie, {
+        method: "PUT",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(changeInfo.cookie.value)
+      })
+      .catch((err) => console.log("err", err))
     }
-});
-
-
-
+  }
+);
